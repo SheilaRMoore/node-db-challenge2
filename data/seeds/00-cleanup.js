843142dcb01209c333f7428e460f4aedc4e9adfb
Sheila R Moore <sherexmykes@github.com>
2020-05-03 23:08:28 -0400

    fixed errors, router, schema, mvp, answered questions

--- data/seeds/00-cleanup.js
+++ data/seeds/00-cleanup.js
@@ -1,23 +1,8 @@
 
-exports.seed = function(knex) {
-  return knex("projects").insert([
-    {
-      id: 1,
-      name: "Go To Work",
-      description: "Make the Bacon",
-      completed: true
-    },
-    {
-      id: 2,
-      name: "Go to School",
-      description: "Allow Your Brain to Grow",
-      completed: false
-    },
-    {
-      id: 3,
-      name: "Sleep",
-      description: "Count Sheep",
-      completed: false
-    }
-  ]);
+exports.seed = async function(knex) {
+  await knex("project_resources").truncate();
+  await knex("task").truncate();
+  await knex("resources").truncate();
+  await knex("projects").truncate();
+
 };
--- data/seeds/01_projects.js
+++ data/seeds/01_projects.js
@@ -1,13 +1,22 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex("projects").insert([
+    {
+      id: 1,
+      name: "Go To Work",
+      description: "Make the Mula",
+      completed: true
+    },
+    {
+      id: 2,
+      name: "Go to School",
+      description: "Expand your Mind",
+      completed: false
+    },
+    {
+      id: 3,
+      name: "Sleep",
+      description: "Nite Nite",
+      completed: false
+    }
+  ]);
 };
